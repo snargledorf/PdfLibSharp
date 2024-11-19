18fb2a800ce715be9be48ca378c0676e7311bcb9
Ryan Esteves <snargledorf@users.noreply.github.com>
2024-11-19 12:01:54 -0500

    Correct the pixel to point conversion using the points per inch to calculate the conversion factor

--- PdfLibSharp/Drawing/Units/UnitPixel.cs
+++ PdfLibSharp/Drawing/Units/UnitPixel.cs
@@ -3,16 +3,18 @@ namespace PdfLibSharp.Drawing.Units;
 public class UnitPixel(int ppi = UnitPixel.DefaultPixelsPerInch) : Unit
 {
     public const int DefaultPixelsPerInch = 96;
+    
+    private readonly double _conversionFactor = PointsPerInch / ppi;
 
     public override double ToPoints(double value)
     {
-        return value * ppi;
+        return value * _conversionFactor;
     }
 
     public override double FromPoints(double points)
     {
-        return points / ppi;
-    }
+        return points / _conversionFactor;
+    } 
 
     public static UnitPixel ForPpi(int ppi = DefaultPixelsPerInch)
     {
