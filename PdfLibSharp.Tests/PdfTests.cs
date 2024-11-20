9f0a3e73194f4c35a4feb08f0c8c9683598a610b
Ryan Esteves <snargledorf@users.noreply.github.com>
2024-11-19 20:10:45 -0500

    Add unit tests for PDF rendering
    Introduced two new unit tests: BasicPdfRender and SubStackRender. These tests ensure that rendering a PDF results in non-empty content by checking the memory stream length.

---
+++ PdfLibSharp.Tests/PdfTests.cs
@@ -0,0 +1,40 @@
+using PdfLibSharp.Elements;
+using PdfLibSharp.Elements.Layout;
+using PdfLibSharp.Rendering;
+
+namespace PdfLibSharp.Tests;
+
+public class PdfTests
+{
+    [Test]
+    public async Task BasicPdfRender()
+    {
+        var pdf = new Pdf();
+
+        IPage page = pdf.AddPage();
+
+        page.AddText("Hello World");
+
+        var memoryStream = new MemoryStream();
+        await PdfRenderer.RenderAsync(pdf, memoryStream);
+        
+        Assert.That(memoryStream.Length, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public async Task SubStackRender()
+    {
+        var pdf = new Pdf();
+
+        IPage page = pdf.AddPage();
+
+        IStackContainer stackContainer = page.AddStack(Direction.Horizontal);
+        
+        stackContainer.AddText("Hello World");
+
+        var memoryStream = new MemoryStream();
+        await PdfRenderer.RenderAsync(pdf, memoryStream);
+        
+        Assert.That(memoryStream.Length, Is.GreaterThan(0));
+    }
+}
