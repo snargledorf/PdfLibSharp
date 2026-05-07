7711eb3ea67f2ddf6048db75b41d8fb926c80f9b
Ryan Esteves <snargledorf@users.noreply.github.com>
2026-05-07 09:42:18 -0400

    Decouple enums from `PdfSharp` dependencies
    Removed `XStringAlignment` and `XLineAlignment` mappings from `StringAlignment` and `LineAlignment` enums to reduce coupling with `PdfSharp`. This simplifies the enums and improves flexibility for future extensions.

--- PdfLibSharp/Elements/StringAlignment.cs
+++ PdfLibSharp/Elements/StringAlignment.cs
@@ -2,7 +2,7 @@ namespace PdfLibSharp.Elements;
 
 public enum StringAlignment
 {
-    Near = XStringAlignment.Near,
-    Center = XStringAlignment.Center,
-    Far = XStringAlignment.Far,
+    Near,
+    Center,
+    Far,
 }
